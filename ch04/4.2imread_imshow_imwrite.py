400dec2bce6c959f29c4ab3ac7ff5a7a23f08613
Xiang Wang <1846468966@qq.com>
2016-04-19 17:47:12 +0800

    修正章节

--- ch04/4.2imread_imshow_imwrite.py
+++ ch04/4.2imread_imshow_imwrite.py
@@ -6,6 +6,6 @@ cv2.imshow('image',img)
 k = cv2.waitKey(0)
 if k == 27:         # wait for ESC key to exit
     cv2.destroyAllWindows()
-elif k == ord('s'): # wait for 's' key to save and exit
+elif k == ord('s')&0xFF: # wait for 's' key to save and exit
     cv2.imwrite('messigray.png',img)
     cv2.destroyAllWindows()
