653b5b065a539ca5ecc13bae8a1716105f01323d
play4fun <play4fun@foxmail.com>
2017-07-12 12:11:01 +0800

    update ch13

--- ch13-颜色空间转换/13.VideoCapture_blue_object.py
+++ ch13-颜色空间转换/13.VideoCapture_blue_object.py
@@ -2,31 +2,48 @@
 import cv2
 import numpy as np
 
+'''
+物体跟踪
+
+• 从视频中获取每一帧图像
+• 将图像转换到 HSV 空间
+• 设置 HSV 阈值到蓝色范围。
+• 获取蓝色物体 当然我们 可以做其他任何我们想做的事 
+比如 在蓝色 物体周围画一个圈。
+
+
+当你学习了【轮廓】之后 你就会学到更多 相关知识
+那是你就可以找到物体的重心 并根据重心来跟踪物体
+仅仅在摄像头前挥挥手就可以画出同的图形，或者其他更有趣的事。
+'''
+
 cap = cv2.VideoCapture(0)
-while (1):
+while True:
     # 获取每一帧
     ret, frame = cap.read()
     # 换到 HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    # 定蓝色的 值
+    # 定蓝色的阈值
     # lower_blue = np.array([110, 50, 50])
     # upper_blue = np.array([130, 255, 255])
 
-    #黑色
+    # 黑色
     lower_black = np.array([0, 0, 0])
     upper_black = np.array([180, 255, 30])
 
-    # 根据 值构建掩模
+    # 根据阈值构建掩模
     # mask = cv2.inRange(hsv, lower_blue, upper_blue)
     mask = cv2.inRange(hsv, lower_black, upper_black)
-    # 对原图像和掩模位 算
+    # 对原图像和掩模位运算
     res = cv2.bitwise_and(frame, frame, mask=mask)
+
     # 显示图像
     cv2.imshow('frame', frame)
     cv2.imshow('mask', mask)
     cv2.imshow('res', res)
-    k = cv2.waitKey(5) & 0xFF
+    k = cv2.waitKey(0)  # & 0xFF
     if k == 27:
         break
 # 关闭窗口
 cv2.destroyAllWindows()
+
--- ch13-颜色空间转换/13.find_object_hsv.py
+++ ch13-颜色空间转换/13.find_object_hsv.py
@@ -2,26 +2,28 @@
 import cv2
 import numpy as np
 
-#wrong
+# wrong
 # green=np.uint8([0,255,0])
 # print green
 # hsv_green=cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
 # print hsv_green
 
 
-#scn (the number of channels of the source),
-#i.e. self.img.channels(), is neither 3 nor 4.
+# scn (the number of channels of the source),
+# i.e. self.img.channels(), is neither 3 nor 4.
 #
-#depth (of the source),
-#i.e. self.img.depth(), is neither CV_8U nor CV_32F.
+# depth (of the source),
+# i.e. self.img.depth(), is neither CV_8U nor CV_32F.
 # 所以不能用 [0,255,0] 而 用 [[[0,255,0]]]
-#的三层括号应 分别对应于 cvArray cvMat IplImage
-green=np.uint8([[[0,255,0]]])
-hsv_green=cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
-print (hsv_green)
-#[[[60 255 255]]]
+# 的三层括号应 分别对应于 cvArray cvMat IplImage
 
-black=np.uint8([[[0,0,0]]])
-hsv_black=cv2.cvtColor(black,cv2.COLOR_BGR2HSV)
-print (hsv_black)
-#[[[0 0 0]]]
+
+green = np.uint8([[[0, 255, 0]]])
+hsv_green = cv2.cvtColor(green, cv2.COLOR_BGR2HSV)
+print(hsv_green)
+# [[[60 255 255]]]
+
+black = np.uint8([[[0, 0, 0]]])
+hsv_black = cv2.cvtColor(black, cv2.COLOR_BGR2HSV)
+print(hsv_black)
+# [[[0 0 0]]]
