6107d70fc70c3be96a0580392ead30bc26b5b81a
play4fun <play4fun@foxmail.com>
2017-07-13 13:51:12 +0800

    update ch28

--- ch28-使用GrabCut算法进行交互式前景提取/grabCut.py
+++ ch28-使用GrabCut算法进行交互式前景提取/grabCut.py
@@ -2,6 +2,16 @@
 __author__ = 'play4fun'
 """
 create time:15-10-25 下午12:20
+
+
+img - 输入图像
+• mask-掩模图像 用来确定 些区域是背景 前景 可能是前景/背景等。 可以 置为 cv2.GC_BGD,cv2.GC_FGD,cv2.GC_PR_BGD,cv2.GC_PR_FGD  或者直接 入 0,1,2,3 也 。
+• rect - 包含前景的矩形 格式为 (x,y,w,h)
+• bdgModel, fgdModel - 算法内 使用的数组. 你只  创建两个大
+小为 (1,65) 数据类型为 np.float64 的数组。
+• iterCount - 算法的迭代次数
+• mode可以 置为cv2.GC_INIT_WITH_RECT或cv2.GC_INIT_WITH_MASK  也可以联合使用。 是用来确定我们  修改的方式 矩形模式或者掩模
+模式。
 """
 
 import numpy as np
@@ -9,12 +19,17 @@ import cv2
 from matplotlib import pyplot as plt
 
 img = cv2.imread('../data/messi5.jpg')
+
 mask = np.zeros(img.shape[:2], np.uint8)
 bgdModel = np.zeros((1, 65), np.float64)
 fgdModel = np.zeros((1, 65), np.float64)
 rect = (50, 50, 450, 290)
+
 # 函数的返回值是更新的 mask, bgdModel, fgdModel
-cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
+cv2.grabCut(img, mask, rect, bgdModel, fgdModel, iterCount=5, mode=cv2.GC_INIT_WITH_RECT)
+#迭代 5 次
+
 mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
 img = img * mask2[:, :, np.newaxis]
+
 plt.imshow(img), plt.colorbar(), plt.show()
--- ch28-使用GrabCut算法进行交互式前景提取/grabCut2.py
+++ ch28-使用GrabCut算法进行交互式前景提取/grabCut2.py
@@ -2,6 +2,12 @@
 __author__ = 'play4fun'
 """
 create time:15-10-25 下午12:20
+实 上我是怎么做的呢 我们使用图像编  件打开 入图像 
+添加一个 图层 
+使用笔刷工具在  的地方使用白色绘制 比如头发  子 球等 
+ 使 用 色笔刷在不  的地方绘制 比如 logo 草地等 。
+ 然后将其他地方用灰 色填充 保存成新的掩码图像。
+ 在 OpenCV 中导入 个掩模图像 根据新的 掩码图像对原来的掩模图像  编 。
 """
 
 import numpy as np
@@ -20,12 +26,12 @@ img = img * mask2[:, :, np.newaxis]
 plt.imshow(img), plt.colorbar(), plt.show()
 
 # newmask is the mask image I manually labelled
-newmask = cv2.imread('../data/newmask.jpg',0)
+newmask = cv2.imread('../data/newmask.jpg', 0)
 # whereever it is marked white (sure foreground), change mask=1
 # whereever it is marked black (sure background), change mask=0
 mask[newmask == 0] = 0
 mask[newmask == 255] = 1
-mask, bgdModel, fgdModel = cv2.grabCut(img,mask,None,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_MASK)
-mask = np.where((mask==2)|(mask==0),0,1).astype('uint8')
-img = img*mask[:,:,np.newaxis]
-plt.imshow(img),plt.colorbar(),plt.show()
+mask, bgdModel, fgdModel = cv2.grabCut(img, mask, None, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_MASK)
+mask = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
+img = img * mask[:, :, np.newaxis]
+plt.imshow(img), plt.colorbar(), plt.show()
