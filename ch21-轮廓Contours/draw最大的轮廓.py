1da3dead7c035bee00d3aa9082b478558baa9981
play4fun <play4fun@foxmail.com>
2017-07-24 16:10:22 +0800

    剪切contour

--- ch21-轮廓Contours/draw最大的轮廓.py
+++ ch21-轮廓Contours/draw最大的轮廓.py
@@ -11,12 +11,13 @@ draw最大的轮廓.py:
 import cv2
 import numpy as np
 
-img22 = cv2.imread('../data/cards.png')
+org = cv2.imread('../data/cards.png')
+img22 = org.copy()
 imgray = cv2.cvtColor(img22, cv2.COLOR_BGR2GRAY)
 cv2.imshow('imgray', imgray)
 
-#白色背景
-ret, threshold = cv2.threshold(imgray, 244, 255, cv2.THRESH_BINARY_INV)#把黑白颜色反转
+# 白色背景
+ret, threshold = cv2.threshold(imgray, 244, 255, cv2.THRESH_BINARY_INV)  # 把黑白颜色反转
 cv2.imshow('after threshold', threshold)
 
 image, contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -28,13 +29,29 @@ for i, cnt in enumerate(contours):
 #
 a2 = sorted(areas, key=lambda d: d[1], reverse=True)
 
-for i, are in a2:
-    if are < 100:
-        continue
-    cv2.drawContours(img22, contours, i, (0, 0, 255), 3)
-    print(i, are)
+# for i, are in a2:
+#     if are < 150:
+#         continue
+#     cv2.drawContours(img22, contours, i, (0, 0, 255), 3)
+#     print(i, are)
+#
+#     cv2.imshow('drawContours', img22)
+#     k=cv2.waitKey(500)
+#     if k==ord('q'):
+#         break
 
-    cv2.imshow('drawContours', img22)
-    cv2.waitKey(0)
+# 获取最大或某个contour，剪切
+idx = a2[1][0]
+mask = np.zeros_like(org)  # Create mask where white is what we want, black otherwise
+cv2.drawContours(mask, contours, idx, (0, 255, 0), -1)  # Draw filled contour in mask
+out = np.zeros_like(org)  # Extract out the object and place into output image
+out[mask == 255] = org[mask == 255]
+cv2.imwrite('out_contour.jpg', out)
+
+#roi方法
+idx = a2[4][0]
+x, y, w, h = cv2.boundingRect(contours[idx])
+roi = org[y:y+h, x:x+w]
+cv2.imwrite('out_contour-roi4.jpg', roi)
 
 cv2.destroyAllWindows()
