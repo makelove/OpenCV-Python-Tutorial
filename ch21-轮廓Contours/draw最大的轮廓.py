ae3133a0f34455bbb7aa356a4849c8681ecb43d5
play4fun <play4fun@foxmail.com>
2017-07-24 16:22:51 +0800

    逐个contour 显示

--- ch21-轮廓Contours/draw最大的轮廓.py
+++ ch21-轮廓Contours/draw最大的轮廓.py
@@ -12,8 +12,8 @@ import cv2
 import numpy as np
 
 org = cv2.imread('../data/cards.png')
-img22 = org.copy()
-imgray = cv2.cvtColor(img22, cv2.COLOR_BGR2GRAY)
+
+imgray = cv2.cvtColor(org, cv2.COLOR_BGR2GRAY)
 cv2.imshow('imgray', imgray)
 
 # 白色背景
@@ -29,16 +29,19 @@ for i, cnt in enumerate(contours):
 #
 a2 = sorted(areas, key=lambda d: d[1], reverse=True)
 
-# for i, are in a2:
-#     if are < 150:
-#         continue
-#     cv2.drawContours(img22, contours, i, (0, 0, 255), 3)
-#     print(i, are)
-#
-#     cv2.imshow('drawContours', img22)
-#     k=cv2.waitKey(500)
-#     if k==ord('q'):
-#         break
+cv2.waitKey(0)
+for i, are in a2:
+    if are < 150:
+        continue
+    img22 = org.copy()#逐个contour 显示
+    cv2.drawContours(img22, contours, i, (0, 0, 255), 3)
+    print(i, are)
+
+    cv2.imshow('drawContours', img22)
+    cv2.moveWindow('drawContours', x=img22.shape[1], y=0)  # 右边
+    k = cv2.waitKey(500)
+    if k == ord('q'):
+        break
 
 # 获取最大或某个contour，剪切
 idx = a2[1][0]
@@ -48,10 +51,10 @@ out = np.zeros_like(org)  # Extract out the object and place into output image
 out[mask == 255] = org[mask == 255]
 cv2.imwrite('out_contour.jpg', out)
 
-#roi方法
+# roi方法
 idx = a2[4][0]
 x, y, w, h = cv2.boundingRect(contours[idx])
-roi = org[y:y+h, x:x+w]
+roi = org[y:y + h, x:x + w]
 cv2.imwrite('out_contour-roi4.jpg', roi)
 
 cv2.destroyAllWindows()
