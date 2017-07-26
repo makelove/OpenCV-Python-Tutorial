a131231c858b17b0e6d4934881a7045a45962fdf
play4fun <play4fun@foxmail.com>
2017-07-26 20:07:27 +0800

    draw canny

--- ch19-Canny边缘检测/19.Canny.py
+++ ch19-Canny边缘检测/19.Canny.py
@@ -32,10 +32,11 @@ from matplotlib import pyplot as plt
 img = cv2.imread('../data/messi5.jpg',0)
 edges = cv2.Canny(img, 100, 200)
 
-plt.subplot(121), plt.imshow(img, cmap='gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+cv2.imshow('Edges',edges)
+cv2.waitKey(0)
 
-plt.subplot(122), plt.imshow(edges, cmap='gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-
-plt.show()
+# plt.subplot(121), plt.imshow(img, cmap='gray')
+# plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+# plt.subplot(122), plt.imshow(edges, cmap='gray')
+# plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+# plt.show()
