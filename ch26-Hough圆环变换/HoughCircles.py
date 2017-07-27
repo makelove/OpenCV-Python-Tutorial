bc402f7d992b17b28353e42f4d58a527b0292f35
play4fun <play4fun@foxmail.com>
2017-07-27 14:33:57 +0800

    检测围棋-白子-黑子

--- ch26-Hough圆环变换/HoughCircles.py
+++ ch26-Hough圆环变换/HoughCircles.py
@@ -4,6 +4,16 @@ __author__ = 'play4fun'
 create time:15-10-25 下午12:02
 一个圆环 需要 3 个参数来确定。所以进行圆环 夫变换的累加器必须是 3 维的
   这样的 效率 就会很低。所以 OpenCV 用来一个比 巧妙的办法 霍夫梯度法 它可以使 用边界的梯度信息。
+  
+参数：
+image： 8位，单通道图像。如果使用彩色图像，请先转换为灰度。
+method：定义检测图像中的圆的方法。目前，唯一实现的方法是cv2.HOUGH_GRADIENT对应于Yuen等。纸。
+dp：该参数是累加器分辨率与图像分辨率的反比（详见Yuen等人）。实质上，dp获取越大，累加器数组越小。
+minDist：检测到的圆的中心（x，y）坐标之间的最小距离。如果minDist太小，则可能（错误地）检测到与原始相邻的多个圆。如果minDist太大，那么一些圈子根本就不会被检测到。
+param1： Yuen等人用于处理边缘检测的梯度值 方法。
+param2：该cv2.HOUGH_GRADIENT方法的累加器阈值。阈值越小，检测到的圈子越多（包括虚假圈子）。阈值越大，可能会返回的圈数越多。
+minRadius：半径的最小大小（以像素为单位）。
+maxRadius：半径的最大大小（以像素为单位）。
 """
 
 import cv2
@@ -13,6 +23,7 @@ img = cv2.imread('../data/OpenCV_Logo_with_text.png', 0)
 img = cv2.medianBlur(img, 5)
 cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
+#HoughCircles(image, method, dp, minDist, circles=None, param1=None, param2=None, minRadius=None, maxRadius=None)
 circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0)
 
 circles = np.uint16(np.around(circles))
---
+++ ch26-Hough圆环变换/HoughCircles_camera.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017/7/27 13:47
+# @Author  : play4fun
+# @File    : HoughCircles_camera.py
+# @Software: PyCharm
+
+"""
+HoughCircles_camera.py:
+"""
+
+import cv2
+import numpy as np
+from skimage.measure import compare_mse as mse
+import string, random
+
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+cap = cv2.VideoCapture(0)
+
+# ret = cap.set(3, 640)
+# ret = cap.set(4, 480)
+
+# margin = 60
+margin = 30
+
+
+def draw_line_rectangle(frame, margin):
+    rows, cols, ch = frame.shape  # (720, 1280, 3)
+    half = int(cols / 2)
+    # 中间
+    cv2.line(frame, (half, 0), (half, rows), (0, 0, 255), 2)
+
+    # margin = 40
+    # 左边
+    up_left1 = (margin, margin)  # 左上点
+    down_right1 = (cols - margin, rows - margin)  # 右下点
+    # print(up_left, down_right)
+    cv2.rectangle(frame, up_left1, down_right1, (0, 255, 0), 3)
+
+
+ret, temp = cap.read()
+tm = 0
+while cap.isOpened():
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
+    if key == ord('s'):
+        cv2.imwrite(id_generator() + '.jpg', frame2)
+
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    m = mse(cv2.cvtColor(temp, cv2.COLOR_BGR2GRAY), cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+    print('mse', m, '----\n')
+    if abs(m - tm) < 2:  # 静止画面，不用重复计算
+        continue
+    else:
+        temp = frame.copy()
+        tm = m
+    #
+    # print(margin,frame.shape[0] - margin, margin,frame.shape[1] - margin)#40 680 40 1240
+    frame2 = frame[margin:frame.shape[0] - margin, margin:frame.shape[1] - margin]  # .copy()
+    # cv2.imshow('frame2', frame2)
+
+    gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    # edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+
+    #HoughCircles(image, method, dp, minDist, circles=None, param1=None, param2=None, minRadius=None, maxRadius=None)
+    # circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0)
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 20, param1=100, param2=30, minRadius=10, maxRadius=40)
+
+    # circles = circles1[0, :, :]  # 提取为二维
+    # circles = np.uint16(np.around(circles1))
+    print(circles)
+    if circles is None:
+        continue
+
+    cimg=frame2
+    for i in circles[0, :]:
+    # for i in circles[:]:
+        # draw the outer circle
+        cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
+        # draw the center of the circle
+        cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+    # cv2.imshow('detected circles', cimg)
+
+    draw_line_rectangle(frame, margin)
+    cv2.imshow("houghlines", frame)
+    # cv2.imwrite('frame3.jpg', frame[margin:frame.shape[0] - margin, margin:frame.shape[1] - margin])
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
