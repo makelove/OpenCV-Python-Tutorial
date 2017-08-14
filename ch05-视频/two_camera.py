f3956ec2f60946f9c434e5bca804301b71594a2f
play4fun <play4fun@foxmail.com>
2017-08-15 00:27:44 +0800

    two_camera 2

--- ch05-视频/two_camera.py
+++ ch05-视频/two_camera.py
@@ -9,6 +9,7 @@ two_camera.py:
 """
 
 import cv2
+import numpy as np
 
 cap0 = cv2.VideoCapture(0)
 cap1 = cv2.VideoCapture(1)
@@ -18,14 +19,17 @@ ret = cap1.set(3, 320)
 ret = cap1.set(4, 240)
 
 while cap0.isOpened() and cap1.isOpened():
-    ret, frame0 = cap0.read()
-    ret, frame1 = cap1.read()
+    ret0, frame0 = cap0.read()
+    ret1, frame1 = cap1.read()
 
-    cv2.imshow('frame0', frame0)
-    cv2.imshow('frame1', frame1)
-    cv2.moveWindow('frame1', x=frame0.shape[1], y=0)
+    if ret0:
+        cv2.imshow('frame0', frame0)
+    if ret1:
+        cv2.imshow('frame1', frame1)
+        # cv2.moveWindow('frame1', x=frame0.shape[1], y=0)
+        cv2.moveWindow('frame1', x=240, y=0)
 
-    key = cv2.waitKey(delay=10)
+    key = cv2.waitKey(delay=2)
     if key == ord("q"):
         break
 
