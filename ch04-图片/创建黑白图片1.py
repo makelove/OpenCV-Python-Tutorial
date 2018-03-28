04b5028534acb2042d85eed20824e9aefe525783
play4fun <play4fun@foxmail.com>
2018-03-28 08:56:00 +0800

    全白

--- ch04-图片/创建黑白图片1.py
+++ ch04-图片/创建黑白图片1.py
@@ -14,8 +14,10 @@ import cv2
 size = (2560, 1600)
 # 全黑.可以用在屏保
 black = np.zeros(size)
+print(black[34][56])
 cv2.imwrite('black.jpg',black)
 
-#white
+#white 全白
 black[:]=255
+print(black[34][56])
 cv2.imwrite('white.jpg',black)
