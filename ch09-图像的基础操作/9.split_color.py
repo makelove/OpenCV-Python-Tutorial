4ad7f4abf9087139dd98e11761608aea80468db6
hongguo <play4fun@foxmail.com>
2018-11-21 18:13:23 +0800

    拆分及合并图像通道

--- ch09-图像的基础操作/9.split_color.py
+++ ch09-图像的基础操作/9.split_color.py
@@ -7,14 +7,16 @@ img=cv2.imread('../data/messi5.jpg')
 
 #
 b,g,r=cv2.split(img)#比较耗时的操作，请使用numpy 索引
-img=cv2.merge(b,g,r)
-
+img=cv2.merge((b,g,r))
 #
 b=img[:,:,0]
 
 #使所有像素的红色通道值都为 0,你不必先拆分再赋值。
 # 你可以 直接使用 Numpy 索引,这会更快。
 img[:,:,2]=0
+
+#保存到文件，看下效果
+cv2.imwrite(filename='split_color2.jpg',img=img)
 
 
 
